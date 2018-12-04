81ab927bd825d3ac80004e2808f5b79d72b8a507
Romserg-Do-IT <s.romankov@do-it.co>
2018-12-04 16:12:14 +0200

    added login and signUp components

--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -1,11 +1,13 @@
 import { AuthTabsComponent } from '../components/auth-tabs/auth-tabs.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
 
 
 @NgModule({
-  declarations: [AuthTabsComponent],
+  declarations: [AuthTabsComponent, LoginComponent, SignupComponent],
   imports: [SharedModule],
-  exports: [AuthTabsComponent]
+  exports: [AuthTabsComponent, LoginComponent, SignupComponent]
 })
 export class AuthModule { }
