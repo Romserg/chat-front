bfac89adbf334251584efbab9844bf003f77ab4b
Romserg <serg.romankov@gmail.com>
2018-12-09 22:33:39 +0200

    can signup new user

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -33,9 +34,38 @@ export class SignupComponent implements OnInit {
 
   matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  constructor(private authService: AuthService, private fb: FormBuilder) { }
+
+  signupForm: FormGroup;
 
   ngOnInit() {
+    this.init();
   }
 
+  init() {
+    this.signupForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(30)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(30)
+      ]]
+    });
+  }
+
+  signupUser() {
+    this.authService.registerUser(this.signupForm.value).subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
+  }
 }
--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -4,11 +4,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from '../components/login/login.component';
 import { SignupComponent } from '../components/signup/signup.component';
+import { AuthService } from '../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AuthTabsComponent, LoginComponent, SignupComponent],
-  imports: [SharedModule, FormsModule, ReactiveFormsModule],
-  exports: [AuthTabsComponent, LoginComponent, SignupComponent]
+  imports: [SharedModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  exports: [AuthTabsComponent, LoginComponent, SignupComponent],
+  providers: [AuthService]
 })
 export class AuthModule { }
