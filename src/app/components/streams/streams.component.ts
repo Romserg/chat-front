3d19bd27b155776851c422c4a1449ff3ca28edfe
Romserg <serg.romankov@gmail.com>
2018-12-16 23:27:58 +0200

    User can logout. Added possibility auto login if token is valid

--- src/app/components/streams/streams.component.ts
+++ src/app/components/streams/streams.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from '../../services/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-streams',
@@ -10,11 +11,15 @@ export class StreamsComponent implements OnInit {
 
   token: any;
 
-  constructor(private tokenService: TokenService) { }
+  constructor(private tokenService: TokenService, private router: Router) { }
 
   ngOnInit() {
     this.token = this.tokenService.GetToken();
     console.log(this.token);
   }
 
+  logout() {
+    this.tokenService.DeleteToken();
+    this.router.navigate(['']);
+  }
 }
--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {StreamsComponent} from '../components/streams/streams.component';
+import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [StreamsComponent],
   imports: [
+    SharedModule,
     CommonModule
   ],
   exports: [StreamsComponent],
