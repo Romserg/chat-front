41f62878b0362849b0a2389e2e626a9fe7d067f3
Romserg <serg.romankov@gmail.com>
2018-12-24 02:01:48 +0200

    created custom file upload component preparing form for sending messages

--- src/app/modules/streams.module.ts
+++ src/app/modules/streams.module.ts
@@ -7,9 +7,10 @@ import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 import { SideComponent } from '../components/side/side.component';
 import { PostFormComponent } from '../components/post-form/post-form.component';
 import { PostsComponent } from '../components/posts/posts.component';
+import { MaterialFileUploadComponent } from '../components/material-file-upload/material-file-upload.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent, PostsComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent, PostsComponent, MaterialFileUploadComponent],
   imports: [
     SharedModule,
     CommonModule
